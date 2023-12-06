3048f51825d15ca09d0151b94783a64d67826541
samtur <108389765+samtur@users.noreply.github.com>
2023-12-06 12:28:33 +0000

    set up directory started writing binary search algo

---
+++ algorithms/binarySearch.js
@@ -0,0 +1,14 @@
+const arr = [1, 2, 3, 5, 8, 20];
+
+function binarySearch(arr) {
+  let mid = 0;
+  let end = arr.length - 1;
+  let start = 0;
+
+  if (arr.length < 0) {
+    return;
+  } else {
+  }
+}
+
+binarySearch(arr);
