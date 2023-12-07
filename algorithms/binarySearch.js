255d680e75197ac907cfd0940a8fef1b9820194a
samtur <108389765+samtur@users.noreply.github.com>
2023-12-07 12:35:47 +0000

    updated binary search

--- algorithms/binarySearch.js
+++ algorithms/binarySearch.js
@@ -1,14 +1,29 @@
 const arr = [1, 2, 3, 5, 8, 20];
+const trgt = 3;
 
-function binarySearch(arr) {
-  let mid = 0;
-  let end = arr.length - 1;
-  let start = 0;
+let mid = Math.floor(arr.length / 2);
+let end = arr.length - 1;
+let start = 0;
 
+function binarySearch(trgt, arr) {
   if (arr.length < 0) {
     return;
-  } else {
+  } else if (arr[mid] === trgt) {
+    console.log(`Your value is at index ${mid}`);
+    return;
+  } else if (trgt < arr[mid]) {
+    end = mid;
+    arr = arr.slice(start, end);
+    mid = Math.floor(arr.length / 2);
+    binarySearch(trgt, arr);
+  } else if (trgt > arr[mid]) {
+    start = mid;
+    arr = arr.slice(start, end);
+    mid = Math.floor(arr.length / 2);
+    binarySearch(trgt, arr);
   }
 }
 
-binarySearch(arr);
+binarySearch(trgt, arr);
+
+// Find out how to return the index of the value in the full array;
